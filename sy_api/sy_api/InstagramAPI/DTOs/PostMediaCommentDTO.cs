76412e4fa4024a257c9774b4f793931a3098ddb4
igorthebarros <1igorthebarros@gmail.com>
2026-03-09 17:59:39 -0300

    create instagram media DTOs

---
+++ sy_api/sy_api/InstagramAPI/DTOs/PostMediaCommentDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace InstagramAPI.DTOs
+{
+    public class PostMediaCommentDTO
+    {
+        public string PostId { get; set; } = string.Empty;
+        public string CommentMessage { get; set; } = string.Empty;
+    }
+}
---
+++ sy_api/sy_api/InstagramAPI/DTOs/PostMediaDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace InstagramAPI.DTOs
+{
+    public class PostMediaDTO
+    {
+        public string InstagramUserId { get; set; } = string.Empty;
+        public string InstagramUserToken { get; set; } = string.Empty;
+        public string URL { get; set; } = string.Empty;
+        public string Caption { get; set; } = string.Empty;
+    }
+}
