67a074ba5dfa5ef91464243e9d3be1f86d1ecbcf
igorthebarros <1igorthebarros@gmail.com>
2026-03-09 17:58:50 -0300

    implement instagram controller endpoints and fixes

--- sy_api/sy_api/InstagramAPI/Controllers/InstagramController.cs
+++ sy_api/sy_api/InstagramAPI/Controllers/InstagramController.cs
@@ -1,82 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InstagramAPI.DTOs;
+using MetaService.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MetaAPI.Controllers
 {
     public class InstagramController : ControllerBase
     {
-        private readonly IConfiguration _config;
-        public InstagramController(IConfiguration config)
+        private readonly IInstagramService _service;
+        public InstagramController(IInstagramService service)
         {
-            _config = config;
+            _service = service;
         }
 
         // TODO: Add cache - [ResponseCache(Duration = 3600)]
         // TODO: Use InstagramPathConstant for endpoints
 
-        #region GET
-
         #region BASICS
         [HttpGet("instagram/id")]
-        public async Task<IActionResult> GetInstagramId()
+        public async Task<IActionResult> GetInstagramAccountId()
         {
-            var token = _config["Instagram:Token"];
-            var url = $"https://graph.instagram.com/me?access_token={token}";
-
-            var client = new HttpClient();
-            var response = await client.GetStringAsync(url);
-
-            return Content(response, "application/json");
-        }
-
-        [HttpGet("instagram")]
-        public async Task<IActionResult> GetInstagram()
-        {
-            var token = _config["Instagram:Token"];
-            var url = $"https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink&access_token={token}";
-
-            var client = new HttpClient();
-            var response = await client.GetStringAsync(url);
-
-            return Content(response, "application/json");
-        }
-        #endregion
-
-        #region PROFILE
-        [HttpGet("instagram/profile-info/basic/{id}")]
-        public async Task<IActionResult> GetInstagramProfileBasicInfo(string id)
-        {
-            var token = _config["Instagram:Token"];
-            var url = $"https://graph.instagram.com/{id}?fields=id,username,name,biography,profile_picture_url,website&access_token={token}";
-
-            var client = new HttpClient();
-            var response = await client.GetStringAsync(url);
-
-            return Content(response, "application/json");
-        }
-
-        [HttpGet("instagram/profile-info/stats/{id}")]
-        public async Task<IActionResult> GetInstagramProfileStatsInfo(string id)
-        {
-            var token = _config["Instagram:Token"];
-            var url = $"https://graph.instagram.com/{id}?fields=followers_count,follows_count,media_count&access_token={token}";
-
-            var client = new HttpClient();
-            var response = await client.GetStringAsync(url);
-
-            return Content(response, "application/json");
-        }
-
-        [HttpGet("instagram/profile-info/business/{id}")]
-        public async Task<IActionResult> GetInstagramProfileBusinessInfo(string id)
-        {
-            var token = _config["Instagram:Token"];
-            var url = $"https://graph.instagram.com/{id}?fields=account_type,business_category_name,category,contact_phone_number,email,address_street,city_name,zip,contact_phone_number&access_token={token}";
-
-            var client = new HttpClient();
-
             try
             {
-                var response = await client.GetStringAsync(url);
+                var response = await _service.GetAccountIdAsync();
 
                 return Content(response, "application/json");
             }
@@ -84,35 +29,211 @@ namespace MetaAPI.Controllers
             {
                 return BadRequest(e.Message);
             }
+        }
 
+        [HttpGet("instagram")]
+        public async Task<IActionResult> GetInstagramAccount()
+        {
+            try
+            {
+                var response = await _service.GetAccountAsync();
+
+                return Content(response, "application/json");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("instagram/profile-info/basic/{id}")]
+        public async Task<IActionResult> GetInstagramProfileBasicInfo(string id)
+        {
+            try
+            {
+                var response = await _service.GetProfileBasicAsync(id);
+
+                return Content(response, "application/json");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("instagram/profile-info/stats")]
+        public async Task<IActionResult> GetInstagramProfileStatsInfo()
+        {
+            try
+            {
+                var response = await _service.GetProfileStatsInfoAsync();
+
+                return Content(response, "application/json");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("instagram/profile-info/business/{id}")]
+        public async Task<IActionResult> GetInstagramProfileBusinessInfo(string id)
+        {
+            try
+            {
+                var response = await _service.GetProfileBusinessInfoAsync();
+
+                return Content(response, "application/json");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
         #endregion
 
-        #region POSTS
+        #region INSTAGRAM POSTS
+        [HttpPost("instagram/post/media-upload")]
+        public async Task<IActionResult> PostInstagramMediaUpload([FromBody] PostMediaDTO media)
+        {
+            try
+            {
+                var response = await _service.GetPostsAsync();
+
+                return Content(response, "application/json");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+            //var token = _config["Instagram:Token"];
+            //media.InstagramUserToken = token;
+            //var url = $"https://graph.instagram.com/{media.InstagramUserId}/media";
+
+            //var content = new FormUrlEncodedContent(new[]
+            //{
+            //    new KeyValuePair<string, string>("access_token", media.InstagramUserToken!),
+            //    new KeyValuePair<string, string>("image_url", media.URL),
+            //    new KeyValuePair<string, string>("is_carousel_item", "false"),
+            //    new KeyValuePair<string, string>("alt_text", "🤖 Testing Meta'\''s IG Graph API...bagulho doido"),
+            //    new KeyValuePair<string, string>("caption", "🤖 Testing Meta'\''s IG Graph API...bagulho doido")
+            //});
+
+            //var client = new HttpClient();
+            //try
+            //{
+            //    var response = await client.PostAsync(url, content);
+
+            //    return Content(await response.Content.ReadAsStringAsync(), "application/json");
+            //}
+            //catch (Exception e)
+            //{
+            //    return BadRequest(e.Message);
+            //}
+        }
+
+        [HttpPost("instagram/post/media-publish")]
+        public async Task<IActionResult> PostInstagramMediaPublish([FromBody] PostMediaDTO media)
+        {
+            try
+            {
+                var response = await _service.GetPostsAsync();
+
+                return Content(response, "application/json");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+            //var token = _config["Instagram:Token"];
+            //media.InstagramUserToken = token;
+            //var url = "https://graph.instagram.com/me/media_publish";
+
+            //var content = new FormUrlEncodedContent(new[]
+            //{
+            //    new KeyValuePair<string, string>("access_token", media.InstagramUserToken),
+            //    new KeyValuePair<string, string>("media_url", media.URL),
+            //    new KeyValuePair<string, string>("creation_id", "18422459899140904"),
+            //    new KeyValuePair<string, string>("caption", media.Caption)
+            //});
+
+            //var client = new HttpClient();
+            //try
+            //{
+            //    var response = await client.PostAsync(url, content);
+
+            //    return Content(await response.Content.ReadAsStringAsync(), "application/json");
+            //}
+            //catch (Exception e)
+            //{
+            //    return BadRequest(e.Message);
+            //}
+        }
+
+        [HttpPost("instagram/post/media-comment")]
+        public async Task<IActionResult> PostInstagramMediaComment([FromBody] PostMediaCommentDTO comment)
+        {
+            try
+            {
+                var response = await _service.GetPostsAsync();
+
+                return Content(response, "application/json");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+            //var token = _config["Instagram:Token"];
+            //var url = $"https://graph.instagram.com/{comment.PostId}/comments";
+
+            //var content = new FormUrlEncodedContent(new[]
+            //{
+            //    new KeyValuePair<string, string>("access_token", token!),
+            //    new KeyValuePair<string, string>("message", comment.CommentMessage),
+
+            //});
+
+            //var client = new HttpClient();
+            //try
+            //{
+            //    var response = await client.PostAsync(url, content);
+
+            //    return Content(await response.Content.ReadAsStringAsync(), "application/json");
+            //}
+            //catch (Exception e)
+            //{
+            //    return BadRequest(e.Message);
+            //}
+        }
+
         [HttpGet("instagram/posts")]
         public async Task<IActionResult> GetInstagramPosts()
         {
-            var token = _config["Instagram:Token"];
-            var accountId = _config["Instagram:AccountId"]; ;
+            try
+            {
+                var response = await _service.GetPostsAsync();
 
-            var url = $"https://graph.instagram.com/{accountId}/media?fields=id,caption,media_url,media_type,permalink&access_token={token}";
-
-            var client = new HttpClient();
-            var response = await client.GetStringAsync(url);
-
-            return Content(response, "application/json");
+                return Content(response, "application/json");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
 
         [HttpGet("instagram/posts/{id}")]
         public async Task<IActionResult> GetInstagramPostsById(string id)
         {
-            var token = _config["Instagram:Token"];
-            var url = $"https://graph.instagram.com/{id}?fields=id,caption,media_url,like_count,comments_count&access_token={token}";
+            try
+            {
+                var response = await _service.GetPostsAsync();
 
-            var client = new HttpClient();
-            var response = await client.GetStringAsync(url);
-
-            return Content(response, "application/json");
+                return Content(response, "application/json");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
         #endregion
 
@@ -120,97 +241,11 @@ namespace MetaAPI.Controllers
         [HttpGet("instagram/insights/{id}")]
         public async Task<IActionResult> GetInstagramInsights(string id)
         {
-            var token = _config["Instagram:Token"];
-            var url = $"https://graph.instagram.com/{id}/insights&access_token={token}";
-
-            var client = new HttpClient();
-            var response = await client.GetStringAsync(url);
-
-            return Content(response, "application/json");
-        }
-        #endregion
-
-        #endregion
-
-        #region POST
-
-        #region INSTAGRAM POSTS
-        [HttpPost("instagram/post/media-upload")]
-        public async Task<IActionResult> PostMediaUpload([FromBody] PostStaticMedia media)
-        {
-            var token = _config["Instagram:Token"];
-            media.InstagramUserToken = token;
-            var url = $"https://graph.instagram.com/{media.InstagramUserId}/media";
-
-            var content = new FormUrlEncodedContent(new[]
-            {
-                new KeyValuePair<string, string>("access_token", media.InstagramUserToken!),
-                new KeyValuePair<string, string>("image_url", media.URL),
-                new KeyValuePair<string, string>("is_carousel_item", "false"),
-                new KeyValuePair<string, string>("alt_text", "🤖 Testing Meta'\''s IG Graph API...bagulho doido"),
-                new KeyValuePair<string, string>("caption", "🤖 Testing Meta'\''s IG Graph API...bagulho doido")
-            });
-
-            var client = new HttpClient();
             try
             {
-                var response = await client.PostAsync(url, content);
+                var response = await _service.GetPostsAsync();
 
-                return Content(await response.Content.ReadAsStringAsync(), "application/json");
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-
-        [HttpPost("instagram/post/media-publish")]
-        public async Task<IActionResult> PostMediaPublish([FromBody] PostStaticMedia media)
-        {
-            var token = _config["Instagram:Token"];
-            media.InstagramUserToken = token;
-            var url = "https://graph.instagram.com/me/media_publish";
-
-            var content = new FormUrlEncodedContent(new[]
-            {
-                new KeyValuePair<string, string>("access_token", media.InstagramUserToken),
-                new KeyValuePair<string, string>("media_url", media.URL),
-                new KeyValuePair<string, string>("creation_id", "18422459899140904"),
-                new KeyValuePair<string, string>("caption", media.Caption)
-            });
-
-            var client = new HttpClient();
-            try
-            {
-                var response = await client.PostAsync(url, content);
-
-                return Content(await response.Content.ReadAsStringAsync(), "application/json");
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-
-        [HttpPost("instagram/post/media-comment")]
-        public async Task<IActionResult> PostMediaComment([FromBody] MediaComment comment)
-        {
-            var token = _config["Instagram:Token"];
-            var url = $"https://graph.instagram.com/{comment.PostId}/comments";
-
-            var content = new FormUrlEncodedContent(new[]
-            {
-                new KeyValuePair<string, string>("access_token", token!),
-                new KeyValuePair<string, string>("message", comment.CommentMessage),
-
-            });
-
-            var client = new HttpClient();
-            try
-            {
-                var response = await client.PostAsync(url, content);
-
-                return Content(await response.Content.ReadAsStringAsync(), "application/json");
+                return Content(response, "application/json");
             }
             catch (Exception e)
             {
@@ -219,6 +254,5 @@ namespace MetaAPI.Controllers
         }
         #endregion
 
-        #endregion
     }
 }
