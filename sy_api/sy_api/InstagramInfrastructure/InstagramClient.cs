93adc67ff944e9945956bd248b6cd01f5fe4feec
igorthebarros <1igorthebarros@gmail.com>
2026-03-09 17:59:24 -0300

    implement Infra and InstagramClient class

--- sy_api/sy_api/InstagramInfrastructure/InstagramClient.cs
+++ sy_api/sy_api/InstagramInfrastructure/InstagramClient.cs
@@ -1,15 +1,32 @@
-﻿using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 using System.Net.Http.Headers;
 
 namespace InstagramInfrastructure
 {
-    public class InstagramClient
+    public interface IInstagramClient
+    {
+        Task<string> GetAccountIdAsync();
+        Task<string> GetAccountAsync();
+        Task<string> GetProfileBasicInfoAsync();
+        Task<string> GetProfileStatsInfoAsync();
+        Task<string> GetProfileBusinessInfoAsync();
+        Task<string> GetPostsAsync();
+        Task<string> GetPostByIdAsync(string postId);
+        Task<string> GetAccountInsightsAsync(string postId);
+        Task<string> CommentAsync(string postId, string message);
+        Task<string> UploadPostAsync(string imageUrl, string caption);
+        Task<string> PublishPostAsync(string creationId);
+    }
+
+    public class InstagramClient : IInstagramClient
     {
         // TODO: Add try catch and logging
         private readonly HttpClient _httpClient;
         private readonly InstagramOptions _options;
+        private readonly IConfiguration _config;
 
-        public InstagramClient(HttpClient http, IOptions<InstagramOptions> opts)
+        public InstagramClient(HttpClient http, IOptions<InstagramOptions> opts, IConfiguration config)
         {
             _httpClient = http;
             _options = opts.Value;
@@ -19,13 +36,24 @@ namespace InstagramInfrastructure
             // TODO: Check the difference without it
             _httpClient.DefaultRequestHeaders.Accept
                 .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _config = config;
         }
 
         private string Format(string route, params object[] args)
             => string.Format(route, args);
 
+        #region BASICS
         public async Task<string> GetAccountIdAsync()
         {
+            var accountId = _config["Instagram:AccountId"];
+
+            if (string.IsNullOrEmpty(accountId))
+            {
+                throw new InvalidOperationException("Instagram Account ID is not configured.");
+            };
+
+            // Add accountId to the URL
+
             var url = Format(
                 InstagramRoutesConstant.INSTAGRAM_ACCOUNT_ID,
                 _options.Token);
@@ -36,8 +64,28 @@ namespace InstagramInfrastructure
             return await response.Content.ReadAsStringAsync();
         }
 
-        public async Task<string> GetProfileBasicAsync(string accountId)
+        public async Task<string> GetInstagramAccountAsync()
         {
+            var url = Format(
+                InstagramRoutesConstant.INSTAGRAM_ACCOUNT,
+                _options.Token);
+
+            var response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+        #endregion
+
+        public async Task<string> GetProfileBasicInfoAsync()
+        {
+            var accountId = _config["Instagram:AccountId"];
+
+            if (string.IsNullOrEmpty(accountId))
+            {
+                throw new InvalidOperationException("Instagram Account ID is not configured.");
+            };
+
             var url = Format(
                 InstagramRoutesConstant.INSTAGRAM_PROFILE_BASIC_INFO,
                 accountId,
@@ -60,6 +108,22 @@ namespace InstagramInfrastructure
             response.EnsureSuccessStatusCode();
 
             return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> GetPostsAsync()
+        {
+            var accountId = _config["Instagram:AccountId"];
+            var token = _config["Instagram:Token"];
+            var path = InstagramRoutesConstant.INSTAGRAM_POSTS;
+
+            var uri = Format(path, _options.AccountId, _options.Token);
+
+            var response = await _httpClient.GetAsync(uri);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+            throw new NotImplementedException();
         }
 
         public async Task<string> CommentAsync(string postId, string message)
@@ -101,13 +165,75 @@ namespace InstagramInfrastructure
             return await response.Content.ReadAsStringAsync();
         }
 
-        public async Task<string> GetPostsAsync()
+        public async Task<string> UploadPostAsync(string imageUrl, string caption)
         {
-            var url = Format(
-                InstagramRoutesConstant.INSTAGRAM_ACCOUNT,
-                _options.Token);
+            var path = InstagramRoutesConstant.INSTAGRAM_POST_UPLOAD;
 
-            var response = await _httpClient.GetAsync(url);
+            var url = Format(path, _options.AccountId);
+
+            var content = new FormUrlEncodedContent(new[]
+            {
+                //new KeyValuePair<string, string>("access_token", media.InstagramUserToken!),
+                //new KeyValuePair<string, string>("image_url", media.URL),
+                new KeyValuePair<string, string>("is_carousel_item", "false"),
+                new KeyValuePair<string, string>("alt_text", "🤖 Testing Meta'\''s IG Graph API...bagulho doido"),
+                new KeyValuePair<string, string>("caption", "🤖 Testing Meta'\''s IG Graph API...bagulho doido")
+            });
+
+            var response = await _httpClient.PostAsync(url, content);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> GetAccountAsync()
+        {
+            var path = InstagramRoutesConstant.INSTAGRAM_ACCOUNT;
+
+            var uri = Format(path, _options.Token);
+
+            var response = await _httpClient.GetAsync(uri);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> GetProfileStatsInfoAsync()
+        {
+            var path = InstagramRoutesConstant.INSTAGRAM_PROFILE_STATS_INFO;
+
+            var uri = Format(path, _options.AccountId, _options.Token);
+
+            var response = await _httpClient.GetAsync(uri);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> GetProfileBusinessInfoAsync()
+        {
+            var path = InstagramRoutesConstant.INSTAGRAM_PROFILE_BUSINESS_INFO;
+
+            var uri = Format(path, _options.AccountId, _options.Token);
+
+            var response = await _httpClient.GetAsync(uri);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> GetAccountInsightsAsync(string postId)
+        {
+            var path = InstagramRoutesConstant.INSTAGRAM_DATA_INSIGHTS;
+
+            var uri = Format(path, _options.AccountId, _options.Token);
+
+            var response = await _httpClient.GetAsync(uri);
+
             response.EnsureSuccessStatusCode();
 
             return await response.Content.ReadAsStringAsync();
--- sy_api/sy_api/InstagramInfrastructure/Program.cs
+++ sy_api/sy_api/InstagramInfrastructure/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Meta Repository is running!");
+﻿using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Meta Repository is running!");
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+var tst = configuration["Instagram:Token"];
